b33774474fc411259c55aa6dfdf1214a69c3da4b
coding-to-music <connors.tom@gmail.com>
2022-05-09 03:42:31 +0000

    Build

--- readPagesViews.js
+++ readPagesViews.js
@@ -1,22 +1,18 @@
-var rp= require('request-promise');
+var rp = require("request-promise");
 
-
-var options={
-  
-    methode: 'GET',    
-    uri:'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/Africa/daily/2017042700/2018051700',
-    json:true    
+var options = {
+  methode: "GET",
+  uri: "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/Africa/daily/2017042700/2018051700",
+  json: true,
 };
 
 rp(options)
-    .then(function(parseBody){
-
-    var data=[];
-    for(i=0 ;i<parseBody.items.length;i++){
-        data.push([parseBody.items[i].timestamp,parseBody.items[i].views]);
+  .then(function (parseBody) {
+    var data = [];
+    for (i = 0; i < parseBody.items.length; i++) {
+      data.push([parseBody.items[i].timestamp, parseBody.items[i].views]);
     }
-          
+
     console.log(data);
-    })
-    .catch(function (err){
-});
+  })
+  .catch(function (err) {});
